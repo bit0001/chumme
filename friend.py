fc79ab5633cc78a66ad94f53e7939075f241e1d3
M <lmiguelvargasf@gmail.com>
2017-01-16 19:31:27 -0500

    feat: Implement method __repr__

--- friend.py
+++ friend.py
@@ -2,3 +2,6 @@ class Friend:
     def __init__(self, name: str, last_name: str):
         self.name = name
         self.last_name = last_name
+
+    def __repr__(self):
+        return 'Name: {0}\nLast Name: {1}'.format(self.name, self.last_name)
