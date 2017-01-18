2218335e258b4a3b4c2d38224b765154c183adb7
M <lmiguelvargasf@gmail.com>
2017-01-17 21:02:04 -0500

    refactor: Create dictionary of queries

--- friend_manager.py
+++ friend_manager.py
@@ -2,31 +2,41 @@ from db_context_manager import DBContextManager
 from friend import Friend
 
 
+QUERIES = {
+    'create_friends_table':
+        """
+        CREATE TABLE IF NOT EXISTS friends
+        (
+          name VARCHAR(50) NOT NULL,
+          last_name VARCHAR(50) NOT NULL)
+        """,
+    'insert_friend':
+        """
+        INSERT INTO friends VALUES (?, ?)
+        """,
+    'get_friends':
+        """
+        SELECT * FROM friends
+        """,
+}
+
+
 class FriendManager:
     def __init__(self, db_path: str):
         self.db_path = db_path
 
         with DBContextManager(db_path) as cursor:
-            cursor.execute("""
-            CREATE TABLE IF NOT EXISTS friends
-            (
-              name VARCHAR(50) NOT NULL,
-              last_name VARCHAR(50) NOT NULL
-            )
-            """)
+            cursor.execute(QUERIES['create_friends_table'])
 
     def add_friend(self, friend: Friend):
         with DBContextManager(self.db_path) as cursor:
-            cursor.execute("""
-            INSERT INTO friends VALUES
-            (?, ?)
-            """, (friend.name, friend.last_name))
+            cursor.execute(
+                QUERIES['insert_friend'],
+                (friend.name, friend.last_name))
 
     def get_friends(self) -> [Friend]:
         with DBContextManager(self.db_path) as cursor:
-            cursor.execute("""
-            SELECT * FROM friends
-            """)
+            cursor.execute(QUERIES['get_friends'])
 
             friends = []
             for row in cursor.fetchall():
