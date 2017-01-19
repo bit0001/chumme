1401b5bec84a61542f4781cf39b366e0a559d35c
M <lmiguelvargasf@gmail.com>
2017-01-19 05:16:59 -0500

    feat: Implement function to get valid input

--- menu.py
+++ menu.py
@@ -64,6 +64,14 @@ class Menu:
         print('Your friend {0} {1} has been added.'.
               format(friend.first_name, friend.last_name))
 
+    @staticmethod
+    def get_valid_input(input_str: str, valid_options: tuple):
+        while True:
+            response = input(input_str)
+            if response in valid_options:
+                return response
+            print('Invalid option')
+
     @new_line
     def modify_friend(self):
         friends = self.friend_manger.get_friends()
