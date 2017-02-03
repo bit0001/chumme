10d16b3e8d3158e3886d0c5f95f42f188899b429
M <lmiguelvargasf@gmail.com>
2017-02-03 01:15:01 -0500

    refactor: Move classes to their own file

--- gui.py
+++ gui.py
@@ -2,12 +2,11 @@ from kivy.app import App
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.listview import ListItemButton
-from kivy.uix.popup import Popup
 
-from database_manager.friend_manager import MinimumFriendParameterException
+from add_update_form import AddFriendForm
+from add_update_form import UpdateFriendForm
 from friend_info import FriendInfoView
 from model.friend import Friend
-from popup import OkPopup
 from utils.getter import get_friend_manager
 from utils.widget import hide_label, show_list_view, show_label, hide_widget
 
@@ -54,68 +53,6 @@ class ChumMeRoot(BoxLayout):
         self.friend_info_view = FriendInfoView()
         self.friend_info_view.update_friend_information(friend)
         self.add_widget(self.friend_info_view)
-
-
-class FriendForm(BoxLayout):
-    friend = ObjectProperty()
-    def __init__(self, friend, **kwargs):
-        self.friend = friend
-        super().__init__(**kwargs)
-
-    def build_friend(self, form):
-        parameters = {
-            'first_name': form.first_name.text,
-            'middle_name': form.middle_name.text,
-            'last_name': form.last_name.text,
-            'birthdate': form.birthdate.text,
-            'email': form.email.text,
-            'cell_phone': form.cell_phone.text
-        }
-        print(parameters)
-        friend = Friend(**parameters)
-
-        return friend
-
-    def display_error_popup(self, action):
-        content = OkPopup(
-            text='First name and last name are mandatory fields.'
-        )
-        content.bind(on_answer=self._on_answer)
-        self.popup = Popup(
-            title='Error {} friend'.format(action),
-            content=content,
-            auto_dismiss=False)
-        self.popup.open()
-
-    def _on_answer(self, instance):
-        self.popup.dismiss()
-
-
-class AddFriendForm(FriendForm):
-    def add_friend(self):
-        friend = self.build_friend(self.parent.add_friend_form)
-        try:
-            get_friend_manager().add_friend(friend)
-        except MinimumFriendParameterException:
-            self.display_error_popup('adding')
-        else:
-            self.parent.show_friend_list()
-
-
-class UpdateFriendForm(FriendForm):
-    def update_friend(self):
-        updated_friend = self.build_friend(self.parent.update_friend_form)
-        try:
-            get_friend_manager().update_friend(updated_friend)
-        except MinimumFriendParameterException:
-            self.display_error_popup('updating')
-        else:
-            self.parent.show_friend_details(updated_friend)
-
-    def build_friend(self, form):
-        friend = super().build_friend(form)
-        friend.id = self.friend.id
-        return friend
 
 
 class FriendList(BoxLayout):
