e430d7314237eaf8a4f6495b959cdda03f21f021
M <lmiguelvargasf@gmail.com>
2017-02-02 23:16:51 -0500

    refactor: Remove update functionality

--- gui.py
+++ gui.py
@@ -1,5 +1,5 @@
 from kivy.app import App
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.listview import ListItemButton
 from kivy.uix.popup import Popup
@@ -33,9 +33,9 @@ class ChumMeRoot(BoxLayout):
             hide_widget(friend_list)
             show_label(no_friends_label, 'There are no friends to show.')
 
-    def show_add_update_friend_form(self, action, friend=None):
+    def show_add_friend_form(self):
         self.clear_widgets()
-        self.add_friend_form = AddFriendForm(friend=friend, action=action)
+        self.add_friend_form = AddFriendForm()
         self.add_widget(self.add_friend_form)
 
     def show_friend_list(self):
@@ -43,12 +43,7 @@ class ChumMeRoot(BoxLayout):
         self.update_friend_list_view()
         self.add_widget(self.friend_list_view)
 
-    def add_update_friend(self, action, friend_id):
-        actions = {
-            'add': get_friend_manager().add_friend,
-            'update': get_friend_manager().update_friend
-        }
-
+    def add_friend(self):
         friend_form = self.add_friend_form
         parameters = {
             'first_name': friend_form.first_name.text,
@@ -59,27 +54,25 @@ class ChumMeRoot(BoxLayout):
             'cell_phone': friend_form.cell_phone.text
         }
 
-        if action.lower() == 'update':
-            parameters['id'] = friend_id
-
         try:
-            actions[action.lower()](Friend(**parameters))
+            get_friend_manager().add_friend(Friend(**parameters))
         except AddFriendError:
             content = OkPopup(
                 text='First name and last name are mandatory fields.'
             )
             content.bind(on_answer=self._on_answer)
 
-            if action[-1] == 'e':
-                action = action[:-1]
-
             self.popup = Popup(
-                title='Error {}ing friend'.format(action.lower()),
+                title='Error adding friend',
                 content=content,
                 auto_dismiss=False)
             self.popup.open()
         else:
             self.show_friend_list()
+
+    def update_friend(self, id):
+        pass
+
 
     def _on_answer(self, instance):
         self.popup.dismiss()
@@ -92,13 +85,8 @@ class ChumMeRoot(BoxLayout):
 
 
 class AddFriendForm(BoxLayout):
-    friend = ObjectProperty()
-    action = StringProperty()
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.friend = kwargs['friend']
-        self.action = kwargs['action'].capitalize()
 
 
 class FriendList(BoxLayout):
