08c5c0d0061af51688f8c9d062456db2ec31a9fa
M <lmiguelvargasf@gmail.com>
2017-02-07 16:02:01 -0500

    refactor: Move ChumMeRoot to its own file

--- controller/chumme.py
+++ controller/chumme.py
@@ -1,51 +1,15 @@
 import os
+from os.path import dirname
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import ObjectProperty
-from kivy.uix.boxlayout import BoxLayout
-from os.path import dirname
 
 import iconfonts
-from controller.add_update_form import AddFriendForm
-from controller.add_update_form import UpdateFriendForm
-from controller.friend_info import FriendInfoView
-from model.friend import Friend
 
 iconfonts.register('default_font',
                    'resources/fontawesome-webfont.ttf',
                    'resources/font-awesome.fontd')
 
-class ChumMeRoot(BoxLayout):
-    add_friend_form = ObjectProperty()
-    update_friend_form = ObjectProperty()
-    friend_list_view = ObjectProperty()
-    friend_info_view = ObjectProperty()
-
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.friend_list_view.update()
-
-    def show_add_friend_form(self):
-        self.clear_widgets()
-        self.add_friend_form = AddFriendForm(friend=Friend())
-        self.add_widget(self.add_friend_form)
-
-    def show_update_friend_form(self, friend):
-        self.clear_widgets()
-        self.update_friend_form = UpdateFriendForm(friend=friend)
-        self.add_widget(self.update_friend_form)
-
-    def show_friend_list(self):
-        self.clear_widgets()
-        self.friend_list_view.update()
-        self.add_widget(self.friend_list_view)
-
-    def show_friend_details(self, friend):
-        self.clear_widgets()
-        self.friend_info_view = FriendInfoView()
-        self.friend_info_view.update_friend_information(friend)
-        self.add_widget(self.friend_info_view)
 
 
 class ChumMeApp(App):
---
+++ controller/chumme_root.py
@@ -0,0 +1,38 @@
+from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+
+from controller.add_update_form import AddFriendForm, UpdateFriendForm
+from controller.friend_info import FriendInfoView
+from model.friend import Friend
+
+
+class ChumMeRoot(BoxLayout):
+    add_friend_form = ObjectProperty()
+    update_friend_form = ObjectProperty()
+    friend_list_view = ObjectProperty()
+    friend_info_view = ObjectProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.friend_list_view.update()
+
+    def show_add_friend_form(self):
+        self.clear_widgets()
+        self.add_friend_form = AddFriendForm(friend=Friend())
+        self.add_widget(self.add_friend_form)
+
+    def show_update_friend_form(self, friend):
+        self.clear_widgets()
+        self.update_friend_form = UpdateFriendForm(friend=friend)
+        self.add_widget(self.update_friend_form)
+
+    def show_friend_list(self):
+        self.clear_widgets()
+        self.friend_list_view.update()
+        self.add_widget(self.friend_list_view)
+
+    def show_friend_details(self, friend):
+        self.clear_widgets()
+        self.friend_info_view = FriendInfoView()
+        self.friend_info_view.update_friend_information(friend)
+        self.add_widget(self.friend_info_view)
