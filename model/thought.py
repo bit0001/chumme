6c361b4393a7deeb6fce3efc51146f0feb744f0e
M <lmiguelvargasf@gmail.com>
2017-02-10 18:17:26 -0500

    feat: Add default value for thought

--- model/thought.py
+++ model/thought.py
@@ -1,4 +1,10 @@
+import datetime
+
+
 class Thought:
-    def __init__(self, text, creation_date):
+    def __init__(self, text, creation_date=None):
         self.text = text
-        self.creation_date = creation_date
+
+        self.creation_date = creation_date if\
+            creation_date else\
+            datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
