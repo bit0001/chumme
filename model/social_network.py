14ef0fae2b606a5bf2643213c5ec8e2432e83345
M <lmiguelvargasf@gmail.com>
2017-02-11 14:25:14 -0500

    chore: Add social networks

--- model/social_network.py
+++ model/social_network.py
@@ -15,6 +15,52 @@ class SocialNetwork(Enum):
         base_url='https://www.facebook.com/',
         logo='facebook.png'
     )
+    TWITTER = SocialNetworkRecord(
+        social_network_name='Twitter',
+        base_url='https://twitter.com/',
+        logo='twitter.png'
+    )
+    LINKEDIN = SocialNetworkRecord(
+        social_network_name='LinkedIn',
+        base_url='https://www.linkedin.com/',
+        logo='linkedin.png'
+    )
+    GOOGLE_PLUS = SocialNetworkRecord(
+        social_network_name='Google+',
+        base_url='https://plus.google.com/',
+        logo='google_plus.png'
+    )
+    INSTAGRAM = SocialNetworkRecord(
+        social_network_name='Instagram',
+        base_url='https://www.instagram.com/',
+        logo='instagram.png'
+    )
+    PINTEREST = SocialNetworkRecord(
+        social_network_name='Pinterest',
+        base_url='https://www.pinterest.com/',
+        logo='pinterest.png'
+    )
+    TUMBLR = SocialNetworkRecord(
+        social_network_name='Tumblr',
+        base_url='https://www.tumblr.com/',
+        logo='tumblr.png'
+    )
+    QUORA = SocialNetworkRecord(
+        social_network_name='Quora',
+        base_url='https://www.quora.com/',
+        logo='quora.png'
+    )
+    STACK_OVERFLOW = SocialNetworkRecord(
+        social_network_name='Stack Overflow',
+        base_url='https://stackoverflow.com/',
+        logo='stack_overflow.png'
+    )
+    GOODREADS = SocialNetworkRecord(
+        social_network_name='Goodreads',
+        base_url='https://www.goodreads.com/',
+        logo='goodreads.png'
+    )
+
 
     def __init__(self, *social_network):
         self.social_network = SocialNetworkRecord(*social_network)
