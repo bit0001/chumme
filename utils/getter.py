141af792fa03b3786f5680a26031a47f8d51a461
M <lmiguelvargasf@gmail.com>
2017-02-11 14:57:12 -0500

    feat: Implement getter for social network manager

--- utils/getter.py
+++ utils/getter.py
@@ -3,6 +3,7 @@ import os
 from database_manager.friend_interest_manager import FriendInterestManager
 from database_manager.friend_manager import FriendManager
 from database_manager.interest_manager import InterestManager
+from database_manager.social_network_manager import SocialNetworkManager
 from database_manager.thought_manager import ThoughtManager
 
 DB_PATH = '{}/{}'.format(
@@ -24,3 +25,7 @@ def get_friend_interest_manager():
 
 def get_thought_manager():
     return ThoughtManager(DB_PATH)
+
+
+def get_social_network_manager():
+    return SocialNetworkManager(DB_PATH)
