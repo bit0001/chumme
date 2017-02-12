69d2367a8501e670637fe9a8689b9c2fdb3131fe
M <lmiguelvargasf@gmail.com>
2017-02-12 18:40:17 -0500

    chore: Create getter for manager

--- utils/getter.py
+++ utils/getter.py
@@ -5,6 +5,7 @@ from database_manager.friend_manager import FriendManager
 from database_manager.friend_social_network_manager import \
     FriendSocialNetworkManager
 from database_manager.interest_manager import InterestManager
+from database_manager.profile_photo_manager import ProfilePhotoManager
 from database_manager.thought_manager import ThoughtManager
 from database_manager.util import DB_PATH
 
@@ -27,3 +28,7 @@ def get_thought_manager():
 
 def get_friend_social_network_manager():
     return FriendSocialNetworkManager(DB_PATH)
+
+
+def get_profile_photo_manager():
+    return ProfilePhotoManager(DB_PATH)
