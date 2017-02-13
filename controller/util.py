c775c70a4c8a3f976a4780839c04d760f42f9382
M <lmiguelvargasf@gmail.com>
2017-02-12 19:11:15 -0500

    fix: Return empty dict if no retrieved data

--- controller/util.py
+++ controller/util.py
@@ -1,4 +1,6 @@
+import io
+
 from kivy.core.image import Image as CoreImage
 
 def get_image_from_blob(blob, extension):
-    return CoreImage(blob, ext=extension)
+    return CoreImage(io.BytesIO(blob), ext=extension)
